d51401dcad8a88117a233c36b1c1006bdb11eff8
murat.acet <murateca@gmail.com>
2018-05-23 23:46:51 +0300

    .vimrc and init.vim moved

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ readonly VIM_PLUG_URI=https://raw.githubusercontent.com/junegunn/vim-plug/master
 
 mkdir -p $AUTOLOAD_PATH
 \curl -fLo $AUTOLOAD_PATH/plug.vim $VIM_PLUG_URI
-cp .vimrc $HOME
+cp .vim/.vimrc $HOME
 vim +PlugInstall +qall
 
 \cp .vim/startify/fortune.vim $PLUGGED_PATH/vim-startify/autoload/startify/
