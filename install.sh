edf36c760fe44b78d55530a7d864fbfcf487b3d7
murat.acet <murateca@gmail.com>
2018-06-02 01:49:08 +0300

    unnecessary backslash chars removed

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ readonly PLUGGED_PATH=$HOME/.vim/plugged
 readonly VIM_PLUG_URI=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p $AUTOLOAD_PATH
-\curl -fLo $AUTOLOAD_PATH/plug.vim $VIM_PLUG_URI
+curl -fLo $AUTOLOAD_PATH/plug.vim $VIM_PLUG_URI
 cp .vim/.vimrc $HOME
 vim +PlugInstall +qall
 
-\cp .vim/startify/fortune.vim $PLUGGED_PATH/vim-startify/autoload/startify/
+cp .vim/startify/fortune.vim $PLUGGED_PATH/vim-startify/autoload/startify/
