86e284f03e1c874df0ec7d62d24b14ed08e5dc2d
Murat Acet <murateca@gmail.com>
2018-12-31 18:58:11 +0300

    install script updated with the new configurations

--- install.sh
+++ install.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 #
-# Install vim-plug and plugins
+#Installs bash, vim and git configurations
 
-readonly AUTOLOAD_PATH=$HOME/.vim/autoload
-readonly PLUGGED_PATH=$HOME/.vim/plugged
-readonly VIM_PLUG_URI=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#bashrc, bash_aliases and bash_git
+if [ -f $HOME/.bashrc ]; then			#if bashrc exists,
+	mv $HOME/.bashrc $HOME/.bashrc.bak	#make it a backup
+fi
+if [ -f $HOME/.bash_aliases ]; then		#same for aliases
+	mv $HOME/.bash_aliases $HOME/.bash_aliases.bak
+fi
 
-mkdir -p $AUTOLOAD_PATH
-curl -fLo $AUTOLOAD_PATH/plug.vim $VIM_PLUG_URI
-cp .vim/.vimrc $HOME
+mkdir $HOME/.bash
+cp .bash/.bash_git $HOME/.bash
+cp .bash_aliases $HOME
+cp .bashrc $HOME
+
+#vimrc, vim-plug and plugin installation
+if [ -f $HOME/.vimrc ]; then
+	mv $HOME/.vimrc $HOME/.vimrc.bak
+fi
+
+readonly VIM_AUTOLOAD=$HOME/.vim/autoload
+
+mkdir -p $VIM_AUTOLOAD
+cp .vim/autoload/plug.vim $VIM_AUTOLOAD
+cp .vimrc $HOME
 vim +PlugInstall +qall
 
-cp .vim/startify/fortune.vim $PLUGGED_PATH/vim-startify/autoload/startify/
+cp .vim/startify/fortune.vim $HOME/.vim/plugged/vim-startify/autoload/startify/
+
+#gitconfig
+if [ -f $HOME/.gitconfig ]; then
+	mv $HOME/.gitconfig $HOME/.gitconfig.bak
+fi
+
